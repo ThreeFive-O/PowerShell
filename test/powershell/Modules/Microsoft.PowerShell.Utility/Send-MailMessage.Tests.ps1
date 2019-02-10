4ea5334bd441db5e85ac73c037b9d1542a1cb1c9
ThreeFive-O <threefive-o.github@outlook.com>
2019-02-10 16:32:39 +0100

    Test without nuget register

--- test/powershell/Modules/Microsoft.PowerShell.Utility/Send-MailMessage.Tests.ps1
+++ test/powershell/Modules/Microsoft.PowerShell.Utility/Send-MailMessage.Tests.ps1
@@ -3,7 +3,7 @@
 
 Describe "Send-MailMessage" -Tags CI {
     BeforeAll {
-        Register-PackageSource -Name nuget.org -Location https://www.nuget.org/api/v2 -ProviderName NuGet
+        #Register-PackageSource -Name nuget.org -Location https://www.nuget.org/api/v2 -ProviderName NuGet -Force
 
         $nugetPackage = "netDumbster"
         Find-Package $nugetPackage -ProviderName NuGet | Install-Package -Scope CurrentUser -Force
